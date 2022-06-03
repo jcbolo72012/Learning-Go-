c25c438bc588e191c4cecc36b14453565accc17a
John Bolognino <jcbolo72012@gmail.com>
2022-06-03 18:46:54 -0400

    Basic server with fiber

--- Go-API-Fiber/main.go
+++ Go-API-Fiber/main.go
@@ -5,22 +5,103 @@ import (
 	// "log"
 	// "strconv"
 	"github.com/gofiber/fiber/v2"
-	"Learning-Go-/Go-API-Fiber/seller"
 )
 
-
-func setupRoutes(app *fiber.App) {
-
-	app.Get("/api/v1/main", seller.GetSellers)
-	app.Get("/api/v1/main/:id", seller.GetSeller)
-	app.Post("/api/v1/main", seller.CreateSeller)
-	app.Delete("/api/v1/main/:id", seller.DeleteSeller)
+type Seller struct{
+	ID string `json:"ID"`
+	Name string `json:"Name"`
+	ASINS string `json:"ASINS"`
+	Rating float64 `json:"rating"`
 }
+
+// func GetSellers(c *fiber.Ctx) {
+// 	c.JSON(sellers)
+// }
+
+// func GetSeller(c *fiber.Ctx) {
+// 	id := c.Params("id")
+// 	for _, item := range sellers {
+// 		if item.ID == id {
+// 			c.JSON(item)
+// 			return
+// 		}
+// 	}
+// }
+
+// func CreateSeller(c *fiber.Ctx) {
+
+// 	seller := new(Seller)
+// 	if err := c.BodyParser(seller); err != nil {
+// 		c.Status(503).Send(err)
+// 		return
+// 	}
+// 	sellers.append(sellers, seller)
+// 	c.JSON(seller)
+	
+// }
+
+// func DeleteSeller(c *fiber.Ctx) {
+// 	id := c.Params("id")
+// 	for index, item := range sellers {
+// 		if item.ID == id {
+// 			sellers = append(sellers[:index], sellers[index+1:]...)
+// 			break
+// 		}
+// 	}
+// 	c.JSON(sellers)
+// }
+
+
+var sellers[]Seller
+
+// func setupRoutes(app *fiber.App) {
+
+// 	app.Get("/api/v1/main", GetSellers)
+// 	app.Get("/api/v1/main/:id", GetSeller)
+// 	app.Post("/api/v1/main", CreateSeller)
+// 	app.Delete("/api/v1/main/:id", DeleteSeller)
+// }
 
 func main() {
 	app := fiber.New()
 
-	setupRoutes(app)
-	fmt.Printf("Starting server at port 8000\n")
+	sellers = append(sellers, Seller{ID: "1", Name:"Charlie", ASINS: "B0009999", Rating: 5})
+
+	app.Get("/api/v1/get_sellers", func(c *fiber.Ctx) error{
+		return c.JSON(sellers)
+	})
+
+	app.Get("/api/v1/get_seller", func(c *fiber.Ctx) error{
+		id := c.Params("id")
+		for _, item := range sellers {
+			if item.ID == id {
+				return c.JSON(item)
+			}
+		}
+		return nil
+	})
+
+	app.Post("/api/v1/create_seller", func(c *fiber.Ctx) error{
+		seller := new(Seller)
+
+		if err := c.BodyParser(seller); err != nil {
+			return err
+		}
+		sellers = append(sellers, *seller)
+		return c.JSON(sellers)
+	})
+
+	app.Delete("/api/v1/delete_seller", func(c *fiber.Ctx) error{
+		id := c.Params("id")
+		for index, item := range sellers {
+			if item.ID == id {
+				sellers = append(sellers[:index], sellers[index+1:]...)
+				return c.JSON(sellers)
+			}
+		}
+		return c.JSON(sellers)
+	})
+	// setupRoutes(app)
+	fmt.Printf("Starting server at port 3000\n")
 	app.Listen(":3000")
 }
